9ec84b70ddfd5a122bcb46e54eec14797d4854a2
wojtaskuba <73549865+wojtaskuba@users.noreply.github.com>
2021-01-27 10:26:10 +0100

    Add files via upload

---
+++ Jakub_Wojtas_zad9_poprawa.py
@@ -0,0 +1,7 @@
+try:
+    with open("plik1.txt", 'r') as f1:
+        with open("plik2.txt",'w') as f2:
+            for line in f1:
+                f2.write(line)
+except IOError as e:
+    print(f"Operation error: {e.strerror}")
